02d8e0093673162106fd4e7e76383b4ebd4ebc0e
ron <ron@myrror.security>
2025-01-14 19:12:22 +0200

    Update alb module

--- terraform/alb/main.tf
+++ terraform/alb/main.tf
@@ -7,6 +7,31 @@ resource "aws_lb" "app_lb" {
   subnets            = var.subnets
 }
 
+# Target Group
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "${var.name}-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+# Listener
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      status_code = "200"
+      content_type = "text/plain"
+      message_body = "OK"
+    }
+  }
+}
+
+
 # Security Group for the Load Balancer
 resource "aws_security_group" "load_balancer_security_group" {
   name        = "load-balancer-security-group"
@@ -24,13 +49,6 @@ resource "aws_security_group" "load_balancer_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 443
-    to_port     = 443
     protocol    = "tcp"
   }
 }
